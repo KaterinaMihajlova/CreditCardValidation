3b2ede875e3d13827cb10018a2a5c8b215a031a6
Katerina <halichakaterina@gmail.com>
2023-05-24 22:41:15 +0200

    Added Client Side Validation

--- public/script.js
+++ public/script.js
@@ -6,6 +6,7 @@ function applyValidation(field, data){
     field.innerHTML = data.message;
   }
 }
+
 function validate()
 {
   var card = document.getElementsByName("card_number");
@@ -18,6 +19,32 @@ function validate()
   var cardCVCError = document.getElementById("cvc_error");
   var cardDateError = document.getElementById("date_error");
   console.log(card[0].value +" "+ cardName[0].value  + " " + cardDate[0].value+ " " + cardCVC[0].value);
+
+  if(!cardName[0].value.match((/^[a-zA-Z]+ [a-zA-Z]+$/))){
+    alert('Please enter a Name and Last Name in the right format.');
+    return ;
+  }
+
+ if (!card[0].value.match(/^4[0-9]{12}(?:[0-9]{3})?$/)&&
+      !card[0].value.match(/^5[1-5][0-9]{14}$/)&&
+      !card[0].value.match(/^3[47][0-9]{13}$/)
+  ) {
+    alert('Please enter a valid credit card number.');
+    return;
+  }
+
+  if(cardDate[0].value == ""){
+    alert('Please enter a valid date.');
+    return;
+  }
+
+  if(!cardCVC[0].value.match(/^\d{3}$/)&&
+     !cardCVC[0].value.match(/^\d{4}$/)
+  ){
+    alert('Please enter a valid CVC/CVV .');
+    return;
+  }
+  
   fetch("/api/validate", {
     method:"POST", 
     body:JSON.stringify({"card": card[0].value, "cardName":cardName[0].value, "cardDate":cardDate[0].value, "cardCVC":cardCVC[0].value, "luhnCard":luhnCard[0].value}), 
